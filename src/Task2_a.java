477ed0fe1f15d146dbf7a296820b152da4361949
Martin Fuchs <martinfuchs11@web.de>
2024-04-30 15:57:10 +0200

    Task 2: Added FileReader and execute.

--- src/Task2_a.java
+++ src/Task2_a.java
@@ -1,2 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task2_a {
+
+    public static void execute () {
+
+    }
+
+    public static List<String> FileReader() {
+        Path path = Paths.get("aoc_2024_day1_puzzle_input.txt");
+        try {
+            return Files.readAllLines(path);
+        } catch (IOException e) {
+            System.out.println("An IO Exception occurred!");
+            return new ArrayList<>();
+        }
+    }
+
+    
+
 }
