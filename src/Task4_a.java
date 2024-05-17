9519596f6959eb89b15ad169f662ed9450fbe59b
Martin Fuchs <martinfuchs11@web.de>
2024-05-17 23:54:12 +0200

    Task 4 a work in progress.

---
+++ src/Task4_a.java
@@ -0,0 +1,85 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class Task4_a {
+
+    public static void execute () {
+        List<String[]> values = getOwnNumbers(getValues(fileReader()));
+        for (String[] current : values) {
+            for (String value : current) {
+                System.out.print(value + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static List<String> fileReader() {
+        Path path = Paths.get("C:\\Users\\marti\\OneDrive\\Desktop/aoc_2024_day4_puzzle_input.txt");
+        try {
+            return Files.readAllLines(path);
+        } catch (IOException e) {
+            System.out.println("An IO Exception occurred!");
+            return new ArrayList<>();
+        }
+    }
+
+    public static List<List<String>> getValues (List<String> game) {
+        List<List<String>> valueList = new ArrayList<>();
+        for (String current : game) {
+            String values = current.substring(current.indexOf(':') + 2);
+            valueList.add(Arrays.stream(values.split(" ")).toList());
+        }
+        return valueList;
+    }
+
+    public static List<List<String>> getWinningNumbers (List<List<String>> values) {
+        List<List<String>> winningNumbers = new ArrayList<>();
+        for (int i = 0; i < values.size(); i++) {
+            List<String> numbers = new ArrayList<>();
+            winningNumbers.add(numbers);
+            int index = 0;
+            while (true) {
+                if (Objects.equals(values.get(i).get(index), "|")) {
+                    break;
+                } else {
+                    winningNumbers.get(i).add(values.get(i).get(index));
+                    index++;
+                }
+            }
+        }
+        return winningNumbers;
+    }
+
+    public static List<List<String>> getOwnNumbers (List<List<String>> values) {
+        List<List<String>> ownNumbers = new ArrayList<>();
+        for (int i = 0; i < values.size(); i++) {
+            List<String> numbers = new ArrayList<>();
+            ownNumbers.add(numbers);
+            int index = 0;
+            while (true) {
+                if (Objects.equals(values.get(i).get(values.get(i).size() - index - 1), "|")) {
+                    break;
+                } else {
+                    ownNumbers.get(i).add(values.get(i).get(values.get(i).size() - index - 1));
+                    index++;
+                }
+            }
+        }
+        return ownNumbers;
+    }
+
+    public static List<List<Integer>> computeMatchingNumbers (List<String[]> winningNumbers, List<String[]> ownNumbers) {
+        List<int[]> matchingNumbers = new ArrayList<>();
+        for (int i = 0; i < winningNumbers.size(); i++) {
+            List<Integer> numbers = new ArrayList<>();
+            for ()
+        }
+    }
+
+}
